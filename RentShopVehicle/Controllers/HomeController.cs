ffa0c4c55e90a8a5df63357c4bc9b7a3bb832468
Dumitru Voicu <dumitru.voicu@isa.utm.md>
2024-02-14 00:24:21 +0200

    added Blog and AboitUs

--- RentShopVehicle/Controllers/HomeController.cs
+++ RentShopVehicle/Controllers/HomeController.cs
@@ -9,7 +9,20 @@ namespace RentShopVehicle.Controllers
     public class HomeController : Controller
     {
         // GET: Home
+        [HttpGet]
         public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult AboutUs()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Blog()
         {
             return View();
         }
