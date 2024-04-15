db5f8df461c9968a4403e80158ea2dd83e685018
Dmitriy Voicu <voiku173dmitry@gmail.com>
2024-04-16 00:55:55 +0300

    authentication filters were added

---
+++ RentShopVehicle/Filters/AuthFilters/AuthAdmin1Attribute.cs
@@ -0,0 +1,35 @@
+﻿using RentShopVehicle.BusinessLogic.Interfaces;
+using RentShopVehicle.Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RentShopVehicle.Filters.AuthFilters
+{
+    public class AuthAdmin1Attribute : ActionFilterAttribute
+    {
+        protected readonly ISession session;
+        public AuthAdmin1Attribute()
+        {
+            var bl = new BusinessLogic.BusinessLogic();
+            session = bl.getSessionS();
+        }
+
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            HttpCookie currentCookies = HttpContext.Current.Request.Cookies["RSV-CC"];
+            if (currentCookies != null)
+            {
+                var currentUser = session.getUserByCookies(currentCookies.Value);
+                if (currentUser == null || currentUser.UserRole != Role.Admin1)
+                {
+                    context.Result = new RedirectToRouteResult(
+                        new System.Web.Routing.RouteValueDictionary(
+                            new { controller = "Error", action = "E404" }));
+                }
+            }
+        }
+    }
+}
---
+++ RentShopVehicle/Filters/AuthFilters/AuthModeratorAttribute.cs
@@ -0,0 +1,38 @@
+﻿using RentShopVehicle.BusinessLogic.Interfaces;
+using RentShopVehicle.Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RentShopVehicle.Filters.AuthFilters
+{
+    public class AuthModeratorAttribute : ActionFilterAttribute
+    {
+        protected readonly ISession session;
+        public AuthModeratorAttribute()
+        {
+            var bl = new BusinessLogic.BusinessLogic();
+            session = bl.getSessionS();
+        }
+
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            HttpCookie currentCookies = HttpContext.Current.Request.Cookies["RSV-CC"];
+            if (currentCookies != null)
+            {
+                var currentUser = session.getUserByCookies(currentCookies.Value);
+                if (currentUser == null ||
+                    currentUser.UserRole != Role.Admin2 ||
+                    currentUser.UserRole != Role.Admin1 ||
+                    currentUser.UserRole != Role.Moderator)
+                {
+                    context.Result = new RedirectToRouteResult(
+                        new System.Web.Routing.RouteValueDictionary(
+                            new { controller = "Error", action = "E404" }));
+                }
+            }
+        }
+    }
+}
